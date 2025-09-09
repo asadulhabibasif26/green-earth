24540d7e40f8cdb9d792a04bbcea21ac33774027
asadulhabibasif26 <eloopazbd@gmail.com>
2025-09-09 18:53:51 +0600

    add to card section js complete

--- script/index.js
+++ script/index.js
@@ -1,13 +1,8 @@
 const loadCategory = () => {
   fetch("https://openapi.programming-hero.com/api/categories")
     .then((res) => res.json())
-    .then((data) => displayCategory(data.categories)
-    );
+    .then((data) => displayCategory(data.categories));
 };
-const activeRemove = () => {
-  const categoryBtn = document.querySelectorAll('.category-btn');
-  categoryBtn.forEach((btn) => btn.classList.remove('active') );
-}
 // const activeRemove =
 const displayCategory = (cets) => {
   const categoriesContainer = document.getElementById("category-title");
@@ -15,37 +10,30 @@ const displayCategory = (cets) => {
     const categoriesTitle = cet.category_name;
     const categoriesId = cet.id;
     const newElement = document.createElement("div");
-    newElement.innerHTML = `<p id="title-no-${cet.id}" onclick="getCategoryCard(${categoriesId})" class="category-btn active btn bg-transparent  border-none  hover:bg-[#15803D] w-[100%] justify-start ">${categoriesTitle}</P>`;
+    newElement.innerHTML = `<p id="title-no-${cet.id}" onclick="getCategoryCard(${categoriesId})" class="category-btn  btn bg-transparent  border-none  hover:bg-[#15803D] w-[100%] justify-start ">${categoriesTitle}</P>`;
     categoriesContainer.append(newElement);
   });
 };
 loadCategory();
-const getCategoryCard = (id) =>{
-  const url = `https://openapi.programming-hero.com/api/category/${id}`
+const getCategoryCard = (id) => {
+  const url = `https://openapi.programming-hero.com/api/category/${id}`;
   fetch(url)
-  .then((res) => res.json())
-  .then((data) => displayCard(data.plants));
+    .then((res) => res.json())
+    .then((data) => displayCard(data.plants));
   loadCard(id);
-}
-
-
+};
 const loadCard = (id) => {
   fetch(`https://openapi.programming-hero.com/api/plants`)
     .then((res) => res.json())
-    .then((data) => {
-      activeRemove();
-      displayCard(data.plants)
-      const clickBtn = document.getElementById(`title-no-${id}`);
-      clickBtn.classList.add('active');
-    });
+    .then((data) => displayCard(data.plants));
 };
 const displayCard = (cards) => {
   const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML ="";
+  cardContainer.innerHTML = "";
   cards.forEach((card) => {
     const newCard = document.createElement("div");
-  newCard.innerHTML = `
-    <div class="shadow-xl p-5 space-y-2 min-h-[430px] w-[343px] rounded-xl bg-white">
+    newCard.innerHTML = `
+    <div class="shadow-xl p-5 space-y-2 min-h-[430px] max-w-[345px] rounded-xl bg-white">
         <div class="">
             <img class="h-[186px] w-[100%] object-cover rounded-xl" src="${card.image}" alt="">
         </div>
@@ -55,11 +43,34 @@ const displayCard = (cards) => {
             <button class="btn rounded-full text-green-400 bg-green-100">${card.category}</button>
             <h2 class="font-semibold"><i class="fa-solid fa-bangladeshi-taka-sign"></i>${card.price}</h2>
         </div>
-        <button class="bg-[#15803D] btn text-white w-full rounded-full">Add to Cart</button>
+        <button onclick="addToCard(${card.id})" class="bg-[#15803D] btn text-white w-full rounded-full">Add to Cart</button>
     </div>
     `;
     cardContainer.append(newCard);
-  })
+  });
 };
 loadCard();
 
+const addToCard = (id) => {
+  const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayAddToCard(data.plants));
+};
+
+const displayAddToCard = (data) => {
+  const addToCardContainer = document.getElementById("add-to-card-container");
+  const newAdd = document.createElement('div');
+  newAdd.innerHTML =`
+  <div class="flex justify-between bg-[#F0FDF4] p-3 rounded-lg">     
+  <div>
+    <h1 class="font-bold">${data.name}</h1>
+    <p><i class="fa-solid fa-bangladeshi-taka-sign"></i>${data.price} x 1</p>
+    </div>
+      <button><i class="fa-solid fa-xmark"></i></button>
+  </div>
+  `
+
+  addToCardContainer.append(newAdd); 
+};
